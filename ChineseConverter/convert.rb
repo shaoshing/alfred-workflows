6c5f9a7fd17ca1ce37bd8c0942055fc665cc80be
Shaoshing <shaoshing@me.com>
2013-10-08 01:54:38 -0400

    [ChineseConverter] Conversion between Trandicational/Simplified Chinese.

---
+++ ChineseConverter/convert.rb
@@ -0,0 +1,29 @@
+# encoding: UTF-8
+load File.expand_path(__FILE__ + "/../tradsim.rb")
+
+def alfred_xml items
+  items_xml = ""
+  items.each_with_index do |item, index|
+    items_xml << <<-XML
+    <item uid="#{index}" arg="#{item[:arg]}">
+      <title>#{item[:title]}</title>
+      <subtitle>#{item[:subtitle]}</subtitle>
+    </item>
+    XML
+  end
+
+  puts <<-XML
+  <items>
+    #{items_xml}
+  </items>
+  XML
+end
+
+items = []
+text = ARGV[0].strip.force_encoding "UTF-8"
+if !text.empty?
+  convertedText = Tradsim.toggle(text)
+  isTheSame = text == convertedText ? "same text" : "text changed"
+  items << {:arg => convertedText, :title => convertedText, :subtitle => isTheSame}
+end
+alfred_xml(items)
